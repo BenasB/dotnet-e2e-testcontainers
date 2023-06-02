2f06c8cb1b30f4a7464be4a6924345326d0bb974
BenasB <29711974+BenasB@users.noreply.github.com>
2023-06-02 19:57:29 +0300

    Initialized ExampleTwo/Web

---
+++ ExampleTwo/Web/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Web.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
